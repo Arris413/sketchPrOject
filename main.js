b40cfb3ed72eb61a4784e3f0cd02ba74ac927960
Arris413 <91161613+Arris413@users.noreply.github.com>
2023-05-11 17:26:29 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+
+timer_counter() 
+    {
+        variable:1;
+    }
+    timer_check()
+    {
+
+    }
+    drawn_sketch()
+    {
+        string
+    }
+    answer_holder()
+    {
+
+    }
+    score()
+    {
+        variable:0;
+    }
+    function setup() {
+        canvas = createCanvas(280, 280);
+        canvas.center();
+        backgroung("orange");
+    }
+
+    function clearCanvas() {
+        background("white");
+    }
